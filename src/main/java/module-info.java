6cc6cd65e6e580b9d47812b37ada47ed80246213
Jonas Westh Nielsen <jwni@itu.dk>
2022-03-10 00:07:38 +0100

    Initial Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.bfst22group27 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.bfst22group27 to javafx.fxml;
+    exports com.example.bfst22group27;
+}
