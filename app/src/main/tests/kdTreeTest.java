e56202b1f07cc9a6aa517b833a0880074202a157
Nicklas Lundsteen Jensen <nlje@itu.dk>
2022-05-09 12:25:20 +0200

    introduce JUnit testing and started kdTree tests

---
+++ app/src/main/tests/kdTreeTest.java
@@ -0,0 +1,18 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import java.beans.Transient;
+
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Test;
+
+public class kdTreeTest {
+
+    @Test
+    public void setUp() {
+
+    }
+
+}
