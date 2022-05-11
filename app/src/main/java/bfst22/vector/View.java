45965a7d1d9b440ba4f918f1c400da28b8ca21fa
Rasmus Nielsen <rasni@itu.dk>
2022-05-11 17:26:02 +0200

    Added application icon

--- app/src/main/java/bfst22/vector/View.java
+++ app/src/main/java/bfst22/vector/View.java
@@ -3,6 +3,7 @@ package bfst22.vector;
 import java.io.IOException;
 
 import javafx.fxml.FXMLLoader;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class View {
@@ -12,10 +13,11 @@ public class View {
 
         primaryStage.show();
         var loader = new FXMLLoader(View.class.getResource("View.fxml"));
+        primaryStage.getIcons().add(new Image(View.class.getResourceAsStream("images/icon.png")));
         primaryStage.setScene(loader.load());
         Controller controller = loader.getController();
         controller.init(model);
-        primaryStage.setTitle("Bornholm");
+        primaryStage.setTitle("MapIT");
         primaryStage.setMaximized(true);
     }
 }
